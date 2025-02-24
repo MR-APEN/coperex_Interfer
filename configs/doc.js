2927d8e3c6508dfed4474823a7401827b4ba52be
MR-APEN <aleapen82@gmail.com>
2025-02-24 15:56:34 -0600

    build:Se creo proyecto se realizan conexiones a la DB y Servidor

---
+++ configs/doc.js
@@ -0,0 +1,27 @@
+import swaggerJSDoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
+
+const swagger = {
+    swaggerDefinition:{
+        openapi:"3.0.0",
+        info:{
+            title: "Coperex Interfer API",
+            version: "1.0.0",
+            description: "API de la empresa Coperex para gestionar la incorporación de nuevos socios y empresas en Interfer",
+            contact:{
+                name: "Javier Apen",
+                email: "japen-2023128@kinal.edu.gt"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3003/coperexInterfer/v1"
+            }
+        ]
+    },
+    apis:[]
+}
+
+const swaggerDocs = swaggerJSDoc(swagger)
+
+export { swaggerDocs, swaggerUi }
